13e98bb64ba753d6e87c13e37466e625473dc106
root <root@debian12.myguest.virtualbox.org>
2024-07-01 17:31:14 +0330

    adding extra translation to crm and its sub-modules

---
+++ custom_addons/crm_fa/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'crm_fa',
+    'version': '1.0',
+    'category': 'Localization',
+    'summary': 'Extra Persian translations for CRM related modules',
+    'description': 'This module contains custom translations for persian/farsi.',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
+
